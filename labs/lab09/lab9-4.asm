7199953372c55ee5ed11e4e449cb58249c6f5f12
tursunov-muhametnazar <1032244472@rudn.university>
2024-12-12 04:37:19 -0800

    add labs

---
+++ labs/lab09/lab9-4.asm
@@ -0,0 +1,37 @@
+%include 'in_out.asm'
+SECTION .data
+msg db "Результат: ",0
+fx: db 'f(x)= 12x - 7',0
+
+SECTION .text
+global _start
+_start:
+mov eax, fx
+call sprintLF
+pop ecx 
+pop edx
+sub ecx,1
+mov esi, 0
+
+next:
+cmp ecx,0h
+jz _end 
+pop eax
+call atoi
+call ffxx
+add esi,eax
+
+loop next
+
+_end:
+mov eax, msg
+call sprint
+mov eax, esi
+call iprintLF
+call quit
+
+ffxx:
+mov ebx,12
+mul ebx
+sub eax,7
+ret
